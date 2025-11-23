69afb64e3188bc5d4d9a9ce9da950bf71bd535d6
Luasdog <cancersot@gmail.com>
2025-11-23 22:58:28 +0800

    Add: 登录、注册界面

---
+++ app/src/main/java/com/example/persona/ui/theme/Theme.kt
@@ -0,0 +1,22 @@
+package com.example.persona.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PersonaTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        content = content
+    )
+}
